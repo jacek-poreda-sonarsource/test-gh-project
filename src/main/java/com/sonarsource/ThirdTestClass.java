3595decae8501a32ee56b3fdb3b49f2000ec6903
Jacek <jacek.poreda@sonarsource.com>
2024-03-12 13:12:53 +0100

    Update ThirdTestClass.java

--- src/main/java/com/sonarsource/ThirdTestClass.java
+++ src/main/java/com/sonarsource/ThirdTestClass.java
@@ -32,6 +32,7 @@ import javax.crypto.spec.SecretKeySpec;
 public class ThirdTestClass {
 
   private static String test = "23124";
+  private static String test213 = "23124";
 
   public String applyCBC(String strKey, String plainText) throws InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
     byte[] bytesIV = "7cVgr5cbdCZVzxczcw5WY".getBytes("UTF-8");
