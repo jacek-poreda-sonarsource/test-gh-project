2432270597f051eb3b0488638eaf7e78ac34b992
Jacek <jacek.poreda@sonarsource.com>
2024-04-08 16:49:30 +0200

    Update FirstTestClass.java

--- src/main/java/com/sonarsource/FirstTestClass.java
+++ src/main/java/com/sonarsource/FirstTestClass.java
@@ -27,4 +27,8 @@ public class FirstTestClass {
     public static String b1 ="test";
     public static String b2 ="test";
     public static String b3 ="test";
+
+    public static String c1 ="test";
+    public static String c2 ="test";
+    public static String c3 ="test";
 }
