cf9f0d70bd98d427b0b2c475bd0bc02ba89af6bf
Jacek <jacek.poreda@sonarsource.com>
2024-05-16 10:00:12 +0200

    Update Main.java

--- src/main/java/com/sonarsource/Main.java
+++ src/main/java/com/sonarsource/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
   private String TEST_VALUE = "123";
 
+  private String TEST_VALUE_qa = "434";
+
   public void main(String[] args) {
     System.out.println("Hello World!");
   }
